91e3300ab0369b548a8c0320108486c62ca8293d
Eric Nguyen <enguyen89141@gmail.com>
2020-02-14 15:47:25 -0800

    added todo functionality; need to show list

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,4 +1,9 @@
-import { LitElement, html } from 'lit-element'; 
+import { LitElement, html } from 'lit-element';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-checkbox';
+import '@vaadin/vaadin-radio-button/vaadin-radio-button';
+import '@vaadin/vaadin-radio-button/vaadin-radio-group';
 
 const VisibilityFilters = {
   SHOW_ALL: 'All',
@@ -6,17 +11,17 @@ const VisibilityFilters = {
   SHOW_COMPLETED: 'Completed'
 };
 
-class TodoView extends LitElement { 
+class TodoView extends LitElement {
 
-  static get properties(){
+  static get properties() {
     return {
-      todo: {type: Array},
-      filter: { type: String},
-      task: {type: String}
+      todo: { type: Array },
+      filter: { type: String },
+      task: { type: String }
     }
   }
 
-  constructor(){
+  constructor() {
     super();
     this.todos = [];
     this.filter = VisibilityFilters.SHOW_ALL;
@@ -25,8 +30,33 @@ class TodoView extends LitElement {
 
   render() {
     return html` 
-      <p>Hello world</p>
+      <div class="input-layout">
+        <vaadin-text-field  
+          placeholder="Task"
+          value="${this.task}"
+          @change="${this.updateTask}">
+        </vaadin-text-field>
+        <vaadin-button
+          theme="primary"
+          @click="${this.addTodo}">
+          Add todo
+          </vaadin-button>
+        </div>
     `;
+  }
+
+  updateTask(e) {
+    this.task = e.target.value;
+  }
+
+  addTodo(){
+    if(this.task){
+      this.todos = [...this.todos, {
+        task: this.task,
+        complete: false
+      }];
+      this.task= "";
+    }
   }
 }
 
